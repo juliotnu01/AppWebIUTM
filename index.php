09f8b5a1a056f2dfb4a00f14ad3d142268bbe868
juliotNu01 <nunezjuliot@gmail.com>
2016-11-18 19:26:05 -0400

    index

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
 <html>
 <head>
 	<title>Aplicación Web</title>
+		<!-- AngularJs --> 										 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+		<!-- jQuery libreria de Bootstrap --> 					 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<!-- Ultima Compilacion de BootStrap --> 				 <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+		<!-- Ultima Compilacion de JavaScript de Bootstrap -->   <script src="css/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div><h1>Aplicacion Web</h1></div>
+<div class="Jumbotron" ><h1>Aplicacion Web</h1></div>
 </body>
 </html>
