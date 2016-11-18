3c8502428b8ffedc23131e2ca0a80b55dbbeb35c
Juliot Nuñez <nunezjuliot@gmail.com>
2016-11-18 19:28:17 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html>
 <head>
 	<title>Aplicación Web</title>
-		<!-- AngularJs --> 										 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
-		<!-- jQuery libreria de Bootstrap --> 					 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-		<!-- Ultima Compilacion de BootStrap --> 				 <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+		<!-- AngularJs --> 					 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+		<!-- jQuery libreria de Bootstrap --> 			 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<!-- Ultima Compilacion de BootStrap --> 		 <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
 		<!-- Ultima Compilacion de JavaScript de Bootstrap -->   <script src="css/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
