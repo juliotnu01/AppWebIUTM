3ac4a5a8e5143d961a4610407b1c27e031e92cb7
juliotNu01 <nunezjuliot@gmail.com>
2016-11-18 20:11:17 -0400

    index

--- index.php
+++ index.php
@@ -2,12 +2,14 @@
 <html>
 <head>
 	<title>Aplicación Web</title>
+		<meta charset="utf-8">
+  		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- AngularJs --> 					 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
 		<!-- jQuery libreria de Bootstrap --> 			 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 		<!-- Ultima Compilacion de BootStrap --> 		 <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
 		<!-- Ultima Compilacion de JavaScript de Bootstrap -->   <script src="css/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div class="Jumbotron" ><h1>Aplicacion Web</h1></div>
+<div class="jumbotron jumbo-jul" ><h1>Aplicacion Web</h1></div>
 </body>
 </html>
