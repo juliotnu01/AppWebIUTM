c0eaa7c60de6936124978c492108b742dd0823c8
juliotNu01 <nunezjuliot@gmail.com>
2016-11-19 01:06:55 -0400

    nuevo

--- index.php
+++ index.php
@@ -10,6 +10,29 @@
 		<!-- Ultima Compilacion de JavaScript de Bootstrap -->   <script src="css/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div class="jumbotron jumbo-jul" ><h1>Aplicacion Web</h1></div>
+	<nav class="navbar navbar-default navbar-fixed-top navbar-jul">
+		  <div class="container-fluid">
+		    	<div class="navbar-header"><a class="navbar-brand" href="#">AppWebIUTM</a></div>
+				    <div class="collapse navbar-collapse">
+					    <ul class="nav navbar-nav">
+					      <li class="navbar-jul"><a href="#">INICIO</a></li>
+					      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">PAGINA 1 <span class="caret"></span></a>
+						        <ul class="dropdown-menu">
+						          <li><a href="#">Pagina 1.1</a></li>
+						          <li><a href="#">Pagina 1.2</a></li>
+						          <li><a href="#">Pagina 1.3</a></li>
+						        </ul>
+					      </li>
+					      <li><a href="#">PAGINA 2</a></li>
+					      <li><a href="#">PAGINA 3</a></li>
+					    </ul>
+					    <ul class="nav navbar-nav navbar-right">
+				        <li>
+				        	<a href="php/SegundaVista.php"><span class="glyphicon glyphicon-log-in"></span> Login </a>
+				        </li>
+				      </ul>
+				  </div>
+		  </div>
+	</nav>
 </body>
 </html>
